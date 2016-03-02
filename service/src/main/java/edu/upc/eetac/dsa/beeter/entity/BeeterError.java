433be19d9037a4be40120f6dcfd40ff0d2e7d208
Carlos Santiago <carlos.san.rol@gmail.com>
2016-03-02 18:11:30 +0100

    Data model added

---
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/entity/BeeterError.java
@@ -0,0 +1,34 @@
+package edu.upc.eetac.dsa.beeter.entity;
+
+/**
+ * Created by carlos on 2/03/16.
+ */
+public class BeeterError {
+
+    private int status;
+    private String reason;
+
+    public BeeterError() {
+    }
+
+    public BeeterError(int status, String reason) {
+        this.status = status;
+        this.reason = reason;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
---
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/entity/Role.java
@@ -0,0 +1,8 @@
+package edu.upc.eetac.dsa.beeter.entity;
+
+/**
+ * Created by carlos on 2/03/16.
+ */
+public enum Role {
+    registered
+}
