4ff837482335e4c955ea10d68804058321542bc0
Carlos Santiago <carlos.san.rol@gmail.com>
2016-03-24 18:26:19 +0100

    HATEOAS added

--- service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterResourceConfig.java
@@ -1,4 +1,5 @@
 package edu.upc.eetac.dsa.beeter;
+import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
@@ -11,5 +12,6 @@ public class BeeterResourceConfig extends ResourceConfig {
         packages("edu.upc.eetac.dsa.beeter");
         packages("edu.upc.eetac.dsa.beeter.auth");
         register(RolesAllowedDynamicFeature.class);
+        register(DeclarativeLinkingFeature.class);
     }
 }
